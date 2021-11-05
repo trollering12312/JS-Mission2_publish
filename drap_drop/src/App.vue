3a90933f697adec801662e155cdf5142539e4019
park young chan <trollering12312@gmail.com>
2021-11-05 15:58:56 +0900

    feat: drag and drop UI

---
+++ drap_drop/src/App.vue
@@ -0,0 +1,108 @@
+<script>
+
+//drag and drop
+import { ref } from 'vue'
+
+export default{
+  setup() {
+      const items = ref([
+          {id: 0, title: 'itemA', list: 1},
+          {id: 1, title: 'itemB', list: 1},
+          {id: 2, title: 'itemC', list: 2}
+      ])
+
+      const getList = (list) => {
+          return items.value.filter((item) => item.list == list)
+      }
+
+      const startDrag = (event, item) => {
+        console.log(item)
+        event.dataTransfer.dropEffect = 'move'
+        event.dataTransfer.effectAllowed = 'move'
+        event.dataTransfer.setData('itemID', item.id)
+      }
+
+      const onDrop = (event, list) => {
+        const itemID = event.dataTransfer.getData('itemID')
+        const item = items.value.find((item) => item.id == itemID)
+        console.log(item)
+        item.list = list
+
+      }
+
+      return{
+          getList,
+          onDrop,
+          startDrag
+      }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div 
+    class='drop-zone' 
+    @drop="onDrop($event, 1)"
+    @dragenter.prevent
+    @dragover.prevent
+    >
+      <div 
+      v-for='item in getList(1)' 
+      :key='item.id' 
+      class='drag-el'
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+
+    <div 
+    class='drop-zone'
+    @drop="onDrop($event, 2)"
+    @dragenter.prevent
+    @dragover.prevent
+    >
+      <div v-for='item in getList(2)' 
+      :key='item.id' 
+      class='drag-el'
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.drop-zone {
+  width: 50%;
+  margin: 50px auto;
+  background-color: gainsboro;
+  padding: 10px;
+  min-height: 10px;
+}
+
+.drag-el {
+  background-color: aqua;
+  color: white;
+  padding: 5px;
+  margin-bottom: 10px;
+}
+
+.drag-el:nth-last-of-type(1){
+  margin-bottom: 0;
+}
+</style>
